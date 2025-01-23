170a4e06bc8a99060fe1eaea95e3b7e31e3c0c7d
CristinaMeli <cristinameli1990@gmail.com>
2025-01-23 03:52:14 +0100

    esercizio svolto lezione 20.01.2025

---
+++ python/giorno0/Cristina Meli - Esercizio 0. 20.01.2025.py
@@ -0,0 +1,21 @@
+#Esercizio 0
+#Scrivere un programma in Python che stampi a schermo il seguente:
+
+#0.la scritta "ciao, mondo!"
+#1.la risultato della somma dei numeri 40 e 2
+#3.il risultato della divisione del numero 40 per il numero 0 ;)
+#Bonus
+#Scrivere un altro programma in Python che stampi a schermo il seguente:
+
+#il contenuto di questo file README.md
+
+print('ciao, mondo!')
+a = 40
+b = 2
+somma = a + b
+print("risultato", somma)
+a = 40
+b = 0
+divisione = a/b
+print('risultato:', divisione)
+
