563f0364b46e11624b127aac003fe0de831fdc81
CristinaMeli <cristinameli1990@gmail.com>
2025-01-28 10:41:49 +0100

    esercizio svolto lezioni 26 e 27.01.2025

--- python/giorno2_3/esercizi/classi/main.py
+++ python/giorno2_3/esercizi/classi/main.py
@@ -1,17 +1,59 @@
 class Country:
-    ...
+      def __init__(self, name):
+          self.name = name
+          self.regions = []
+
+      def add_region(self, region):
+          self.regions.append(region)
+      @property
+      def pop(self):
+          total_population = sum(region.pop for region in self.regions)
+          return total_population
+          """The total population of this country"""
+
+      @property
+      def most_populuous_city(self):
+          most_populuous_city = None  # ancora non è un dato noto
+          for region in self.regions:
+              for city in region.cities:
+                  if most_populuous_city is None or (city.pop and city.pop > most_populuous_city.pop):
+                      most_populuous_city = city
+          return most_populuous_city
+
+italy = Country("Italy")
+italy.name
+
+class Region:
+    def __init__(self, name):
+        self.name = name
+        self.cities = []
+
+    def add_city(self, city):
+        self.cities.append(city)
 
     @property
     def pop(self):
-        """The total population of this country"""
+        total_population = sum(city.pop for city in self.cities if city.pop)
+        return total_population
 
-
-class Region:
-    ...
-
+sicily = Region("Sicily")
+sicily.name
+italy.add_region(sicily)
 
 class City:
-    """A city"""
-
     def __init__(self, name, pop=None):
-        ...
+        self.name = name
+        self.pop = pop
+
+catania = City("Catania", pop=300_000)
+palermo = City("Palermo", pop=600_000)
+
+sicily.add_city(catania)
+sicily.add_city(palermo)
+
+calabria = Region("Calabria")
+calabria.name
+italy.add_region(calabria)
+reggio_calabria = City("Reggio Calabria", pop=170_000)
+calabria.add_city(reggio_calabria)
+
--- python/giorno2_3/esercizi/funzioni/main.py
+++ python/giorno2_3/esercizi/funzioni/main.py
@@ -1,4 +1,34 @@
 # Scrivere il codice dell'esercizi qui dentro
 
 def pow_list(seq):
-    ...
+    return [x ** 2 for x in seq]
+
+def count_words(str):
+    return "hello world" .split(" ")
+
+def reverse_string(hello):
+    return hello [:: - 1]
+
+def factorial (n):
+    return 1 if n == 0 else n * factorial(n-1)
+
+assert factorial (5) == 120
+
+def is_palindrome(racecar):
+    return racecar == racecar [:: - 1]
+
+def sum_even_numbers(numbers):
+    return sum(num for num in numbers if num % 2 == 0)
+
+def find_max(numbers):
+    return max(numbers)
+
+def count_vowels(helloworld):
+    vowels = "aeiou"
+    return sum(1 for char in helloworld if char.lower() in vowels)
+
+
+
+
+
+
